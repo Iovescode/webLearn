794be620960b739284e09f8f1e7f8239813ce522
lovescode <13022195532@126.com>
2018-10-21 22:24:47 +0800

    l

--- element-admin-devs/src/utils/http/xhr.js
+++ element-admin-devs/src/utils/http/xhr.js
@@ -1,20 +1,10 @@
-/**
- * 将对象转成 a=1&b=2的形式
- * @param obj 对象
- */
+
 function obj2String(obj, arr = [], idx = 0) {
   for (const item in obj) {
     arr[idx++] = [item, obj[item]]
   }
   return new URLSearchParams(arr).toString()
 }
-
-/**
- * 真正的请求
- * @param url 请求地址
- * @param options 请求参数
- * @param method 请求方式
- */
 function commonFetcdh(url, options, method = 'GET') {
   const searchStr = obj2String(options)
   let initObj = {}
@@ -42,20 +32,10 @@ function commonFetcdh(url, options, method = 'GET') {
   })
 }
 
-/**
- * GET请求
- * @param url 请求地址
- * @param options 请求参数
- */
 function GET(url, options) {
   return commonFetcdh(url, options, 'GET')
 }
 
-/**
- * POST请求
- * @param url 请求地址
- * @param options 请求参数
- */
 function POST(url, options) {
   return commonFetcdh(url, options, 'POST')
 }
